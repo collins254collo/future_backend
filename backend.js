a3c1e30943b838b75447fadcfd36b5223cdbf2ab
Collins Njogu <njogucollins10397@gmail.com>
2025-10-18 14:20:51 +0300

    connecting with frontend

--- backend.js
+++ backend.js
@@ -8,8 +8,14 @@ const contactRouter = require('./router/contactrouter.js');
 const app = express();
 const port = 8080;
 
-//  Middleware
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: "https://collins-wamiatu.vercel.app",
+    credentials: true, 
+  })
+);
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads'));
